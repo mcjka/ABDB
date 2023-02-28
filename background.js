25d16fb5bbb79ccedceecae690efaf3da26c1bc8
mcjka <95513381+mcjka@users.noreply.github.com>
2023-02-27 17:11:41 -0800

    Update background.js

--- background.js
+++ background.js
@@ -1,3 +1,13 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
 	chrome.tabs.executeScript(null, {file: "changeDom.js"});
+	var request = new XMLHttpRequest(); 
+	request.open("GET", 'http://www.url.com');
+	request.onreadystatechange = function() { 
+	if (request.readyState === 4 && request.status === 200) {
+
+	//response handling code
+
+	}
+	};
+	request.send(null); // Send the request now
 });
