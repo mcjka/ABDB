f4d0f2fd67218f986fdc927eb4bf35592ca291df
mcjka <95513381+mcjka@users.noreply.github.com>
2023-02-28 17:43:51 -0800

    Update background.js

--- background.js
+++ background.js
@@ -1,5 +1,20 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
 	chrome.tabs.executeScript(null, {file: "changeDom.js"});
 	chrome.tabs.executeScript(null, {file: "downloadPage.js"});
+	function onStartedDownload(id) {
+    		console.log(`Started to download: ${id}`);
+  	}
+
+  	function onFailed(error) {
+    		console.log(`Something stinks: ${error}`);
+  	} 
+
+  	var startDownload = browser.downloads.download({
+    		url : currentUrl,
+    		filename: 'samplePage.html'
+ 	 });
+
+  	startDownload.then(onStartedDownload, onFailed);
+	
 	
 });
